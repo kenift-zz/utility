7cbc4bcc280f1d4fe128adb97e16cbc10d5b6488
Paulo Vernetti <vernetti@kenift.com>
2020-06-14 20:13:55 +0300

    test: adding tests for methods getDifferenceBetweenNumbers

--- src/maths/getDifferenceBetweenNumbers.ts
+++ src/maths/getDifferenceBetweenNumbers.ts
@@ -1,13 +1,7 @@
-import {Exception} from "@kenift/exception";
-
 export function getDifferenceBetweenNumbers(num1: number, num2: number) {
-  if (typeof num1 === "number" && typeof num2 === "number") {
-    if (num1 > num2) {
-      return num1 - num2;
-    } else {
-      return num2 - num1;
-    }
+  if (num1 > num2) {
+    return num1 - num2;
   } else {
-    throw Exception.error("Utility", "getDifferenceBetweenNumbers method parameters should be a number type");
+    return num2 - num1;
   }
 }
