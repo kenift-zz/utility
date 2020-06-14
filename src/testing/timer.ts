cbee09ebe8d2a8a31686f20a19d4c24f81c19a24
Paulo Vernetti <vernetti@kenift.com>
2020-06-15 02:24:29 +0300

    fix: reworking types of stop method

--- src/testing/timer.ts
+++ src/testing/timer.ts
@@ -1,18 +1,24 @@
-import {Exception} from "@kenift/exception";
+import {Exception, KExceptionInfo} from "@kenift/exception";
 
 export class Timer {
-  public stop: (options: Kenift.Utility.TestingTimerStopOptions) => void;
+  public stop: (options: {
+    module?: string;
+    message?: string;
+  }) => KExceptionInfo;
   constructor() {
     let start = Date.now();
 
-    this.stop = (options: Kenift.Utility.TestingTimerStopOptions) => {
+    this.stop = (options: {
+      module?: string;
+      message?: string;
+    }): KExceptionInfo => {
       let finish = Date.now();
       let diff = finish - start;
       let seconds = diff / 1000;
 
       let text = options.message ? options.message : "?";
 
-      Exception.info(
+      return Exception.info(
         options.module ? options.module : "Unknown",
         seconds + " seconds to " + text
       )
