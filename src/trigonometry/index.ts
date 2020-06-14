4351cb77cb7326e48680411541c58691c42a3369
Paulo Vernetti <vernetti@kenift.com>
2020-06-14 20:16:34 +0300

    refactor: renaming getDistanceBetweenCoords.ts to getDistanceBetweenCoords2D.ts
    File contain function to get distance only with 2D Coordinate System

--- src/trigonometry/index.ts
+++ src/trigonometry/index.ts
@@ -1,5 +1,6 @@
 import {degreesToRadians} from './degreesToRadians';
 import {getPointCoordsAtDegree} from './getPointCoordsAtDegree';
+import {getDistanceBetweenCoords2D} from "./getDistanceBetweenCoords2D";
 
 export const Trigonometry = new class Trigonometry {
   getPointCoordsAtDegree(options: Kenift.Utility.GetPointCoordsAtDegreeOptions): Kenift.Utility.GetPointCoordsAtDegreeResult {
@@ -7,5 +8,8 @@ export const Trigonometry = new class Trigonometry {
   }
   degreesToRadians(degrees: number): number {
     return degreesToRadians(degrees);
+  }
+  getDistanceBetweenCoords2D(options: Kenift.Utility.getDistanceBetweenCoords2DOptions): number {
+    return getDistanceBetweenCoords2D(options);
   }
 }
